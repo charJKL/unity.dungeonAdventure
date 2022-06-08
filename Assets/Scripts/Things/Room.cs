b378f360470db0fd3736b57676fe0aea3f159543
charJKL <11600464+charJKL@users.noreply.github.com>
2022-06-08 15:42:18 +0200

    Move `Room` script to `Thing` namespace.

---
+++ Assets/Scripts/Things/Room.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Things
+{
+	public class Room : MonoBehaviour
+	{
+		[SerializeField] private Camera cameraObject;
+		
+		[HideInInspector] public GoThroughEvent[] doors;
+		[HideInInspector] public PickupEvent[] items;
+		
+		void Awake()
+		{
+			doors = GetComponentsInChildren<GoThroughEvent>();
+			items = GetComponentsInChildren<PickupEvent>();
+			
+			Debug.Log("Awake room " + this.name + " with doors= "+ doors.Length);
+		}
+		
+		public void ViewRoom()
+		{
+			cameraObject.enabled = true;
+		}
+		
+		public void ExitRoom()
+		{
+			cameraObject.enabled = false;
+		}
+	}
+}
