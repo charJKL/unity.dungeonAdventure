82fbf293b38bffc5991d25cbb2c72221ac029620
charJKL <11600464+charJKL@users.noreply.github.com>
2022-06-08 16:06:54 +0200

    Add helping Gizmos for doors showing passages.

--- Assets/Scripts/Things/Door.cs
+++ Assets/Scripts/Things/Door.cs
@@ -7,5 +7,15 @@ namespace Things
 	public class Door : MonoBehaviour
 	{
 		[SerializeField] public Room destination;
+		
+		void OnDrawGizmos()
+		{
+			// Draw door destination:
+			if(destination != null)
+			{
+				Gizmos.color = Color.yellow;
+				Gizmos.DrawLine(transform.position, destination.transform.position);
+			}
+		}
 	}
 }
